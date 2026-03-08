6493b617b614d357d82e5251a6d9626bb1c676f2
Misbah Kamate <misbahkamate18@gmail.com>
2026-03-08 12:18:39 +0530

    Add files via upload

---
+++ primenumber.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main ()
+{
+	int num,i,count=0;
+	
+	printf("ENTER A NUMBER :");
+	scanf("%d",&num);
+	
+	for(i=1;i<=num;i++)	
+	{
+		if(num%i==0)
+		{
+			count++;
+		}
+	}
+	
+	if(count==2)
+	{
+		printf("%d is a Prime number.",num);
+	}
+	else{
+		printf("%d is not a Prime number .",num);
+	}
+	return 0;
+}
